c22a4156d77d6716833837af7c9e09f539d20870
nazarstodolya <gnz1991@gmail.com>
2022-03-21 17:18:58 +0200

    js_homework

--- JS/lesson9/homework/js9.js
+++ JS/lesson9/homework/js9.js
@@ -31,12 +31,12 @@ for (let i = 0; i < array.length; i++) {
 }
 //- Є масив
 // let coursesAndDurationArray = [
-//     {title: 'JavaScript Complex', monthDuration: 5},
-//     {title: 'Java Complex', monthDuration: 6},
-//     {title: 'Python Complex', monthDuration: 6},
-//     {title: 'QA Complex', monthDuration: 4},
-//     {title: 'FullStack', monthDuration: 7},
-//     {title: 'Frontend', monthDuration: 4}
+// {title: 'JavaScript Complex', monthDuration: 5},
+// {title: 'Java Complex', monthDuration: 6},
+// {title: 'Python Complex', monthDuration: 6},
+// {title: 'QA Complex', monthDuration: 4},
+// {title: 'FullStack', monthDuration: 7},
+// {title: 'Frontend', monthDuration: 4}
 // ];
 // Для кожного елементу масиву зробити блок в якому вивести
 // інформацію про title та monthDuration
